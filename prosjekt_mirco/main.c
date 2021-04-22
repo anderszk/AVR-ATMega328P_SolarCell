2bd2f4d0fadf40cfc259964869dca006cc735ca9
nixxolai <82967068+nixxolai@users.noreply.github.com>
2021-04-22 16:38:03 +0200

    ...

---
+++ prosjekt_mirco/main.c
@@ -0,0 +1,18 @@
+/*
+ * prosjekt_mirco.c
+ *
+ * Created: 22.04.2021 16:29:27
+ * Author : nixxo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
