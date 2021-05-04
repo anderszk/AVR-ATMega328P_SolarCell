1cf57859ba8a82e95b0c39cdf0e5d26cdd7618ec
Anders Kristensen <71840284+anderszk@users.noreply.github.com>
2021-05-04 17:04:36 +0200

    k

--- prosjekt_mirco/main.c
+++ prosjekt_mirco/main.c
@@ -1,29 +1,65 @@
 /*
-* prosjekt_mirco.c
-*
-* Created: 22.04.2021 16:29:27
-* Author : nixxo
+ * prosjekt_mirco.c
+ *
+ * Created: 22.04.2021 16:29:27
+ * Author : nixxo
+ 
+ 
+ test wwwooooowwww
+ */ 
 
-*/
 
+
+
+/* Quick interactive demo running servo with Tohmer 1 */
+
+// ------- Preamble -------- //
 #define F_CPU 16000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-int pos = 90;   // initial position
-int tolerance = 2;
+#define PULSE_MIN 1000 /* experiment with these values */
+#define PULSE_MAX 2000 /* to match your own servo */
+#define PULSE_MID 1500
+
+
+static inline void initADC0(void) {
+	ADMUX |= (1 << REFS0); //reference voltage on AVCC
+	ADCSRA |= (1 << ADPS1) | (1 << ADPS0); //ADC clock prescaler /8
+	ADCSRA |= (1 << ADEN); //enables the ADC
+}
+
+
+static inline void initTimer1Servo(void) {
+	/* Set up Timer1 (16bit) to give a pulse every 20ms */
+	/* Use Fast PWM mode, counter max in ICR1 */
+	TCCR1A |= (1 << WGM11);
+	TCCR1B |= (1 << WGM12) | (1 << WGM13);
+	TCCR1B |= (1 << CS10); /* /1 prescaling -- counting in microseconds */
+	ICR1 = 20000; /* TOP value = 20ms */
+	DDRB |= (1 << PORTB1); /* set pin for output */
+	TCCR1A |= (1 << COM1A1); /* Direct output on PB1 / OC1A */
+}
 
 
 int main(void) {
 	
-	DDRC = 0b00000000; // setter A0-A8 til input
-	DDRB = (1<<PORTB1); // setter pin9 til output
+	// -------- Inits --------- //
+	
+	// adc:
+	uint16_t potentiometerValue;
+	initADC0();
 
-	while(1){
+	//servo:
+	OCR1A = PULSE_MID; /* set it to middle position initially */
+	_delay_ms(500);
+	initTimer1Servo();
+	
+	
+	// ------ Event loop ------ //
+	
+	while (1) {
 		
-	}
-
-	return (0); /* This line is never reached */
 }
--- prosjekt_mirco/servo.c
+++ prosjekt_mirco/servo.c
@@ -1,137 +1,21 @@
-<<<<<<< HEAD
 include "servo.h"
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
 #define F_CPU 16000000UL // 16MHz system clock
-=======
-
-// Defines
-
-#define F_CPU 16000000UL // 16MHz system clock
-
->>>>>>> ce1a85a3c5e74628c127a636687a6bf60f0ebe57
 #define T1_PRESCALE 8 // Timer 1 clock prescaler
 
-/* System clock and timer prescaler will give one tick each 0,5us, thus we
-
-need 40000 ticks to get 20 ms period, 1 ms is 2000 and 2 ms is 4000.
-
-Code below automatically calculates values.*/
-
 // servo period = 20ms = 20000us (=50Hz):
-
 #define SERVO_PERIOD F_CPU/1000000*20000/T1_PRESCALE
 
-// maximum servo should be = 2ms = 2000us, but is 2100 after testing servo:
-
-#define SERVO_MAX F_CPU/1000000*2100/T1_PRESCALE
-
-// minimum should be = 1ms = 1000us, but is 460 us after testing:
-
+#define SERVO_MAX F_CPU/1000000*2000/T1_PRESCALE
 #define SERVO_MIN F_CPU/1000000*460/T1_PRESCALE
 
-// Include definition file(s)
-
-#include <avr/interrupt.h>
-<<<<<<< HEAD
-#include <util/delay.h>
-
-=======
-
-#include <util/delay.h>
-
-// Prototypes
-
-void ADC_Init(void);
-
-void Timer1_PWM_init(void);
-
-/* Main program. */
->>>>>>> ce1a85a3c5e74628c127a636687a6bf60f0ebe57
-
-int main( void )
-
-{
-
-<<<<<<< HEAD
-ADC_Init();
-
-Timer1_PWM_init();
-
-// OCR1A = SERVO_MIN; // Manual setting to 0Â° for testing
-
-// OCR1A = (SERVO_MAX+SERVO_MIN)/2; // Manual setting to 90Â° for testing
-
-// OCR1A = SERVO_MAX; // Manual setting to 180Â° for testing
-
-/* Endless loop. Starts ADC measurement and waits for it to finish
-
-Adjusts servo signal according to ADC measurement. */
-
-do {
-
-
-ADCSRA |= (1<<ADSC); // Start ADC conversion.
-
-do {} while (ADCSRA & (1<<ADSC)); // wait for it to finish
-
-OCR1A = ADC*(SERVO_MAX-SERVO_MIN)/1024+SERVO_MIN;
-
-// OCR1A = ((ADC*3)+SERVO_MIN); // quick approximation of servo value
-
-} while(1);
-
-}
-
 //Timer initialization
-=======
-	ADC_Init();
-
-	Timer1_PWM_init();
-
-	// OCR1A = SERVO_MIN; // Manual setting to 0° for testing
-
-	// OCR1A = (SERVO_MAX+SERVO_MIN)/2; // Manual setting to 90° for testing
-
-	// OCR1A = SERVO_MAX; // Manual setting to 180° for testing
-
-	/* Endless loop. Starts ADC measurement and waits for it to finish
-
-	Adjusts servo signal according to ADC measurement. */
-
-	do {
-
-		// Test code for finding endpoints
-
-		// OCR1A = SERVO_MIN;
-
-		// _delay_ms(1000);
-
-		// OCR1A = SERVO_MAX;
-
-		// _delay_ms(1000);
-
-		ADCSRA |= (1<<ADSC); // Start ADC conversion.
-
-		do {} while (ADCSRA & (1<<ADSC)); // wait for it to finish
-
-		OCR1A = ADC*(SERVO_MAX-SERVO_MIN)/1024+SERVO_MIN;
-
-		// OCR1A = ((ADC*3)+SERVO_MIN); // quick approximation of servo value
-
-	} while(1);
-
-}
-
-/* Timer 1 PWM initialization. */
-
->>>>>>> ce1a85a3c5e74628c127a636687a6bf60f0ebe57
-void Timer1_PWM_init(void)
+void Timer1_PWM_init(int time)
 
 {
 
-<<<<<<< HEAD
 DDRB = (1<<DDB1); // Set OC1A/PB1 as output.
 
 /* Fast PWM non-inverting mode. Clear OC1A on Compare Match, set at TOP=ICR1.
@@ -160,40 +44,9 @@ ADCSRA = (1<<ADEN)|(7<<ADPS0);
 }
 
 //Moves servo to desired position
-void moveServo(int position){
-    return
-=======
-	DDRB = (1<<DDB1); // Set OC1A/PB1 as output.
+void moveServo(int sens1, int sens2, int potmeter){
+    //Map her
 
-	/* Fast PWM non-inverting mode. Clear OC1A on Compare Match, set at TOP=ICR1.
-
-	Pre-scale 16Mhz by 8 -> 2MHz = 0.5us/tick */
-
-	TCCR1A = (1<<COM1A1)|(0<<COM1A0)|(1<<WGM11)|(0<<WGM10);
-
-	TCCR1B = (1<<CS11)|(1<<WGM13)|(1<<WGM12);
-
-	ICR1 = SERVO_PERIOD; // Servo period = 20ms (50Hz)
-
+    OCR1A = ADC*(SERVO_MAX-SERVO_MIN)/1024+SERVO_MIN;
 }
 
-/*! ADC initialization. Pre-scaler 128x, resulting in 125kHz ADC clock @ 16MHz.
-
-ADC0 input. AVCC as reference. Result left-adjusted.
-
-*/
-
-void ADC_Init(void)
-
-{
-
-	// ADCSRB = 0x00; // Free running mode.
-
-	ADMUX = (1<<REFS0)|(0<<ADLAR); // ADC0 single ended input and result right adjusted.
-
-	DIDR0 |= (1<<ADC0D); // digital input buffer disable, saves power.
-
-	ADCSRA = (1<<ADEN)|(7<<ADPS0);
-
->>>>>>> ce1a85a3c5e74628c127a636687a6bf60f0ebe57
-}
