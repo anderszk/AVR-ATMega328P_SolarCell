8791c73556db656bbf24498759a323d54a3b1780
nixxolai <82967068+nixxolai@users.noreply.github.com>
2021-05-05 08:14:10 +0200

    ....

--- prosjekt_mirco/sensorlogic.c
+++ prosjekt_mirco/sensorlogic.c
@@ -1,16 +1,6 @@
 #include "sensorlogic.h"
 
 
-
-int potmeter_init(){
-    return;
-}
-
-int lightsensor1_init(){
-    
-}
-
-
 void validatePosition(int position){ //Checks id the position has exceeded the valid positions
     if (position < 0){
         position = 0;
@@ -21,6 +11,9 @@ void validatePosition(int position){ //Checks id the position has exceeded the v
 }
 
 int correctPosition(int potValue, int leftSensor, int rightSensor){ //Returns degrees
+	
+	 potValue = potValue/200;
+	 
     if (leftSensor > rightSensor){ //Må finne ut av hvilke vei den skal være
         return (position -= potValue);
     }
