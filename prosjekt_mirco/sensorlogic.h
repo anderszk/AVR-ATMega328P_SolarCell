89f9f14076ed0fd4f6b5537997e3058057b842e8
Anders Kristensen <71840284+anderszk@users.noreply.github.com>
2021-05-07 20:02:37 +0200

    ...

--- prosjekt_mirco/sensorlogic.h
+++ prosjekt_mirco/sensorlogic.h
@@ -1,9 +1,11 @@
 #ifndef SENSORLOGIC_H_INCLUDED
 #define SENSORLOGIC_H_INCLUDED
 
+//Includes
 #include <stdint.h>
 #include <stdbool.h>
 
+//Function that can be used in main
 bool validatePosition(uint8_t position);
 bool validateTolerance(int16_t val1, int16_t val2);
 int correctPosition(int16_t potValue, int16_t leftSensor, int16_t rightSensor, uint8_t position);
