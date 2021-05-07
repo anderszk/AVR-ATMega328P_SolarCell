a4e1571b36796beee1a53088c05332398263aae3
Anders Kristensen <71840284+anderszk@users.noreply.github.com>
2021-05-07 20:02:53 +0200

    jkl

--- prosjekt_mirco/sensorlogic.c
+++ prosjekt_mirco/sensorlogic.c
@@ -1,35 +1,32 @@
-// justerer servo basert på sensor og potensometer
+//Includes
 #include "sensorlogic.h"
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <stdlib.h>
 
-//#define F_CPU 16000000UL // 16MHz system clock
+//Defines
 #define T1_PRESCALE 8 // Timer 1 clock prescaler
 #define SERVO_PERIOD F_CPU/1000000*20000/T1_PRESCALE
-#define SERVO_MAX F_CPU/1000000*2550/T1_PRESCALE   /
+#define SERVO_MAX F_CPU/1000000*2550/T1_PRESCALE   
 #define SERVO_MIN F_CPU/1000000*500/T1_PRESCALE  
 #define POSITION_MAX 180
 
-//Timer initialization
-void Timer1_PWM_init(){
+void Timer1_PWM_init(){ //Timer initialization
 	
-	DDRB = (1<<DDB1); // Setter PB1 som output. (pin 9)
+	DDRB = (1<<DDB1); // Sets PB1 as output
 
+	//Settings for servo
 	TCCR1A = (1<<COM1A1)|(0<<COM1A0)|(1<<WGM11)|(0<<WGM10);
-
 	TCCR1B = (1<<CS11)|(1<<WGM13)|(1<<WGM12);
 
 	ICR1 = SERVO_PERIOD; // Periodetid på 20ms (50Hz)
 }
 
 
-//ADC
-int ADC_Conversion(int8_t pin){
+int ADC_Conversion(int8_t pin){ //Analog to digital converter @param pinnumber to determine MUX(input pin)
 
-	if (pin == 0){
+	if (pin == 0){ //Sets reference voltage to supply voltage (5V)
 		ADMUX = (1<<REFS0)|(0<<ADLAR)|(0 << MUX1)|(0 << MUX0); // ADC0
 	}
 	else if(pin == 1){
@@ -42,24 +39,24 @@ int ADC_Conversion(int8_t pin){
 		return 0;
 	}
 	
-	ADCSRA |= (1<<ADSC);
+	ADCSRA |= (1<<ADSC); //Starts the conversion
 	
 	do {} while (ADCSRA & (1<<ADSC)); // vent på start-convertion (vent så lenge bittet er høy)
 	return ADC;
 }
 
-void ADC_Init(void){
-	ADCSRA = (1<<ADEN)|(0b111<<ADPS0);
-}
-
-//kjører servoen
-void moveServo(uint8_t position){
-	OCR1A = (((position*(SERVO_MAX - SERVO_MIN))/POSITION_MAX) + SERVO_MIN);
+void ADC_Init(void){ //Initialize the ADC
+	ADCSRA = (1<<ADEN)|(0b111<<ADPS0); //Enables ADC with prescale = 8
 }
 
 
+void moveServo(uint8_t position){ //@param desired position, Converts position to period and sends output with the timer
+	OCR1A = (((position*(SERVO_MAX - SERVO_MIN))/POSITION_MAX) + SERVO_MIN); //Map-function to convert position to period
+}
 
-bool validatePosition(uint8_t position){     // gjør sånn at posisjonen til servo holder seg mellom 0-180 grader
+
+
+bool validatePosition(uint8_t position){ //@param desired position, validates the position to prevent invalid outoput to servo
 	if (!(position <= 5 || position >= 180)){
 		return true;
 	}
@@ -69,8 +66,8 @@ bool validatePosition(uint8_t position){     // gjør sånn at posisjonen til se
 }
 
 
-bool validateTolerance(int16_t val1, int16_t val2){
-	int tolleranse = 5;
+bool validateTolerance(int16_t val1, int16_t val2){ //Checks if the delta is above the tolerance to determine if the servo should move
+	int tolleranse = 5; //Delta must be >5 to move
 	
 	if((abs(val1-val2) <= tolleranse) || (abs(val2-val1) <= tolleranse)){
 		return false;
@@ -81,9 +78,9 @@ bool validateTolerance(int16_t val1, int16_t val2){
 }
 
 
-int correctPosition(int16_t potValue, int16_t leftSensor, int16_t rightSensor, uint8_t position){   //returnerer grader til servo
+int correctPosition(int16_t potValue, int16_t leftSensor, int16_t rightSensor, uint8_t position){   //Gives the determined position of the solarpanel
 	
-	potValue = (potValue+100)/200;
+	potValue = (potValue+100)/200; //The rate of which the solarpanel should move (in degrees)
 	
 	if (leftSensor > rightSensor){     
 		return (position -= potValue);
