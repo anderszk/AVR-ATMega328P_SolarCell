58698e5c635a568dc0378ac4c39ba25cbc6bb902
nixxolai <82967068+nixxolai@users.noreply.github.com>
2021-05-07 15:20:02 +0200

    ....

--- prosjekt_mirco/main.c
+++ prosjekt_mirco/main.c
@@ -17,30 +17,67 @@
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
+
+uint8_t position = 90;
+uint16_t potMeter;
+uint16_t lightSensor1;
+uint16_t lightSensor2;
+
+
+
+
+
+
+
 void readPin(int pin){
-	printString("Sensorvalue: ");
 	printWord(pin);
 	printString("\r\n");
 }
 
+ISR(INT0_vect) {
+	if (bit_is_clear(PIND, PD2)) {
+		printString("Poteniometer value: ");
+		readPin(potMeter);
+		printString("Lightsensor 1 value: ");
+		readPin(lightSensor1);
+		printString("Lightsensor 2 value: ");
+		readPin(lightSensor2);
+		printString("Position: ");
+		readPin(position);
+		//_delay_ms(1000);
+	}
+	else{
+		
+	}
+}
+
+void initInterrupt0(void) {
+	PORTD |= (1 << PD2);    // pull up
+	EIMSK |= (1 << INT0);                                 /* enable INT0 */
+	EICRA |= (1 << ISC00);                /* trigger when button changes */
+	sei();                          /* set (global) interrupt enable bit */
+}
+
 int main(void) {
-	
-	uint8_t position = 90;
-	uint16_t potMeter;
-	uint16_t lightSensor1;
-	uint16_t lightSensor2;
 	
 	UBRR0 = BAUD_PRESCALE;
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 	
+	// ------- Inits -------- //
 	initUSART();
 	ADC_Init();
 	Timer1_PWM_init();
+	initInterrupt0();
+
+	 
+	 
 	
+	
+	// ------- Handler -------- //
 	do{
 		
 		lightSensor1 = ADC_Conversion(0);
-		lightSensor2 = ADC_Conversion(1);;
+		lightSensor2 = ADC_Conversion(1);
 		potMeter = ADC_Conversion(2);
 
 		position = correctPosition(potMeter, lightSensor1, lightSensor2, position);
@@ -54,22 +91,6 @@ int main(void) {
 		else if (position < 5){
 			position = 1;
 		}
-		
-		
-		readPin(position);
-		
-		
-		
-		//Interrupts
-		
-		
-		/* For testing
-		moveServo(1);
-		_delay_ms(30000);
-		moveServo(179);
-		_delay_ms(30000);
-		*/
-
 	} while(1);
 }
 
